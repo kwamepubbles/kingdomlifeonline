d6c44fbbaf3d9f383a8d539f332968a5bb5fe2fc
Elvis Taylor <taylorelvisedem@gmail.com>
2023-01-07 15:20:57 +0000

    structural fixes

--- components/header.php
+++ components/header.php
@@ -1,6 +1,7 @@
 <?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,56 +13,44 @@
   <link href="assets/fontawesome6/css/fontawesome.css" rel="stylesheet">
   <link href="assets/fontawesome6/css/brands.css" rel="stylesheet">
   <link href="assets/fontawesome6/css/solid.css" rel="stylesheet">
-  <link  href="assets/bootstrap-icons/font/bootstrap-icons.css"rel="stylesheet">
+  <link href="assets/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/css/bootstrap.min.css" rel="stylesheet">
   <script src="assets/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/popper.min.js"></script>
-  
 </head>
 
 <body>
-  <nav class="navbar sticky-top navbar-expand-lg shadow">
+  <nav class="navbar sticky-top navbar-expand-lg bg-dark shadow">
     <div class="container">
-      <a class="navbar-brand" href="index.php"><img src="./img/klm-HD.png" width="30" height="24" class="d-inline-block align-text-center rounded-circle"></a>
+      <a class="navbar-brand" href="index.php"><img src="./img/klm-HD-icon.png" class="d-inline-block align-text-center rounded-circle"></a>
       <p>KINGDOM LIFE</p>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav ms-auto">
+        <ul class="navbar-nav m-auto">
           <li class="nav-item">
-            <i class="bi bi-house"></i>
-            <a class="nav-link" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link" aria-current="page" href="index.php"><i class="bi bi-house"></i>Home</a>
           </li>
           <li class="nav-item">
-            <i class="bi bi-headphones"></i>
-            <a class="nav-link" href="podcast.php">Podcast</a>
+            <a class="nav-link" href="podcast.php"><i class="bi bi-headphones"></i>Podcast</a>
           </li>
           <li class="nav-item">
-            <i class="fas fa-camera"></i>
-            <a class="nav-link" href="gallery.php">Gallery</a>
+            <a class="nav-link" href="gallery.php"><i class="fas fa-camera"></i>Gallery</a>
           </li>
           <li class="nav-item">
-            <i class="bi bi-book"></i>
-            <a class="nav-link" href="library.php">Library</a>
+            <a class="nav-link" href="library.php"><i class="bi bi-book"></i>Library</a>
           </li>
           <li class="nav-item">
-            <i class="fas fa-praying-hands"></i>
-            <a class="nav-link" href="pray.php">Pray</a>
+            <a class="nav-link" href="pray.php"><i class="fas fa-praying-hands"></i>Pray</a>
           </li>
           <li class="nav-item">
-            <i class="fas fa-seedling"></i>
-            <a class="nav-link" href="support.php">Seeds & Offering</a>
+            <a class="nav-link" href="support.php"><i class="fas fa-seedling"></i>Seeds & Offering</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="#"><i class="fas fa-fax"></i>Contact</a>
           </li>
         </ul>
-        <nav class="navbar ms-auto">
-          <ul class="navbar-nav">
-            <li class="nav-item ">
-              <i class="fas fa-fax"></i>
-              <a class="nav-link" href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </div>
   </nav>
