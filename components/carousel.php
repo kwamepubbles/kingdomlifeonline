9ed980e47421009be5ad85feba7de83a7d213f75
Elvis Taylor <taylorelvisedem@gmail.com>
2023-01-11 22:35:55 +0000

    fixed overflow issues

--- components/carousel.php
+++ components/carousel.php
@@ -1,50 +1,48 @@
 <!-- slider -->
-<div class="container-fluid">
-  <div class="p-0 m-0">
-    <?php
-    $stmt = $plug->prepare("SELECT * FROM carousel");
-    $stmt->execute();
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    $carousel_images = $stmt->fetchAll();
-    ?>
-    <div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel">
-      <ol class="carousel-indicators">
-        <?php
-        $i = 0;
-        foreach ($carousel_images as $image) {
-          if ($i == 0) {
-            echo '<li data-bs-target="#carouselIndicators" data-bs-slide-to="' . $i . '" class="active"></li>';
-          } else {
-            echo '<li data-bs-target="#carouselIndicators" data-bs-slide-to="' . $i . '"></li>';
-          }
-          $i++;
+<div class="container-fluid p-0 m-0">
+  <?php
+  $stmt = $plug->prepare("SELECT * FROM carousel");
+  $stmt->execute();
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $carousel_images = $stmt->fetchAll();
+  ?>
+  <div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <?php
+      $i = 0;
+      foreach ($carousel_images as $image) {
+        if ($i == 0) {
+          echo '<li data-bs-target="#carouselIndicators" data-bs-slide-to="' . $i . '" class="active"></li>';
+        } else {
+          echo '<li data-bs-target="#carouselIndicators" data-bs-slide-to="' . $i . '"></li>';
         }
-        ?>
-      </ol>
-      <div class="carousel-inner">
-        <?php
-        $i = 0;
-        foreach ($carousel_images as $image) {
-          if ($i == 0) {
-            echo '<div class="carousel-item active">';
-          } else {
-            echo '<div class="carousel-item">';
-          }
-          echo '<img class="d-block image-fluid" width="100%" height="400px" src="' . "gallery/" . $image['image_path'] . '" alt="' . $image['image_title'] . '">';
-          echo '</div>';
-          $i++;
+        $i++;
+      }
+      ?>
+    </ol>
+    <div class="carousel-inner">
+      <?php
+      $i = 0;
+      foreach ($carousel_images as $image) {
+        if ($i == 0) {
+          echo '<div class="carousel-item active">';
+        } else {
+          echo '<div class="carousel-item">';
         }
-        ?>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+        echo '<img class="d-block image-fluid" width="100%" height="400px" src="' . "gallery/" . $image['image_path'] . '" alt="' . $image['image_title'] . '">';
+        echo '</div>';
+        $i++;
+      }
+      ?>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
 </div>
--- index.php
+++ index.php
@@ -10,10 +10,10 @@ include('components/carousel.php');
 <section>
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-lg-6 col-md-6 col-sm about">
-                <h2 class="font-weight-bold text-center me-auto"> Welcome to Kingdom Life</h2>
-                <div class="col-lg-12 col-md-12 col-sm inner p-3 mb-3">
-                    <p class="text-wrap me-auto">
+            <div class="col-lg col-md col-sm about">
+                <h2 class="font-weight-bold text-center mx-auto "> Welcome to Kingdom Life</h2>
+                <div class="col inner p-3 mb-3">
+                    <p class="mx-auto">
                         We are a community of believers dedicated to spreading the love and message of Jesus Christ. We strive to create an atmosphere of acceptance, kindness, and support.
                         Our fellowship acivities include a mix of traditional and contemporary worship, prophetic ministrations as well as expounding of scripture. We also offer a variety of small group Bible studies and prayer partnerships.
                         If you are new to KINGDOM LIFE, we invite you to come and check us out. Our doors are always open and we would love to have you join us in worship and fellowship.
@@ -25,15 +25,15 @@ include('components/carousel.php');
                         Evangelism Team.
                     </p>
                     <div class="row justify-content-center">
-                        <div class="col-8 pb-3 m-2">
+                        <div class="col-4 pb-3 mx-auto">
                             <a href="#" class="btn btn-secondary font-weight-bold ">GET IN TOUCH WITH US</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm">
-                <div class="justify-content-center">
-                    <img class="d-block img-fluid" width="70%" src="img/klm-HD.png" alt="Image">
+            <div class="col-lg col-md col-sm">
+                <div class="row justify-content-center">
+                    <img class="d-block w-60 h-auto" src="img/klm-HD.png" alt="...">
                 </div>
             </div>
         </div>
