1eb39dfd1478d9d91fb12a982ea6e59881c30edc
Elvis Taylor <taylorelvisedem@gmail.com>
2023-01-12 23:44:20 +0000

    styling fix

--- pray.php
+++ pray.php
@@ -3,6 +3,13 @@ include('components/header.php');
 ?>
 <section class="pray">
     <div class="container-fluid">
+        <div class=" row justify-content-center">
+            <div class="col-12 text-bg-light pt-4">
+                <div class="p-2 text-center">
+                    <h2><i class="fas fa-praying-hands"></i><br>Let's Pray </h2>
+                </div>
+            </div>
+        </div>
         <div class="row justify-content-center">
             <div class="col-3 p-0 m-0">
                 <img src="img/3.jpeg" class="img-fluid" alt="...">
@@ -19,27 +26,24 @@ include('components/header.php');
         </div>
         <div class="container-fluid pb-4">
             <div class="row justify-content-center">
-                <div class="col-6 pt-5">
+                <div class="col-sm-12 col-md-8 col-lg-6 pt-5">
                     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="$_POST">
-                        <div class="border shadow p-3 m-0">
-                            <div class="p-2 text-center">
-                                <h2><i class="fas fa-praying-hands"></i><br>Let's Pray </h2>
-                            </div>
+                        <div class=" p-3 m-auto">
                             <div class="form-floating ">
-                                <div class=" col-3 mb-3 input-group">
+                                <div class=" col mb-3 input-group">
                                     <span class="input-group-text">First Name</span>
                                     <input type="text" aria-label="First name" class="form-control" required>
                                 </div>
-                                <div class=" col-3 mb-3 input-group">
+                                <div class=" col mb-3 input-group">
                                     <span class="input-group-text">Last Name</span>
                                     <input type="text" aria-label="Last name" class="form-control" required>
                                 </div>
-                                <div class="col-3 mb-3  input-group">
+                                <div class="col mb-3  input-group">
                                     <span class="input-group-text">Contact</span>
                                     <input type="text" aria-label="contact" class="form-control" placeholder="E.g 024XXXXXXX" required>
                                 </div>
                             </div>
-                            <div class="col-sm mb-3">
+                            <div class="col mb-3">
                                 <textarea class="form-control " cols="20" rows="3" placeholder="Enter Prayer Request here" required></textarea>
                             </div>
                             <div class="row justify-content-center">
--- support.php
+++ support.php
@@ -4,75 +4,66 @@ include('components/header.php');
 <div class="support-bg">
     <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col-lg-8 col-md-8 col-sm mt-auto">
-                <div class="card shadow">
-                    <h2 class="text-center pt-2"><i class="fas fa-seedling"></i><br>Seeds & Offering </h2>
-                    <br>
-                    <div class="">
+            <div class="col-12 text-bg-light pt-4">
+                <div class="p-2 text-center">
+                    <h2><i class="fas fa-seedling"></i><br>Seeds & Offering </h2>
+                </div>
+            </div>
+        </div>
 
-                        <div class="row justify-content-center">
-                            <div class="col-2 p-3">
-                                <img src="img/momo.jpg" class="img-fluid" alt="...">
-                            </div>
-                            <div class="col-2">
-                                <img src="img/AirtelTigo-Money.png" class="img-fluid" alt="...">
-                            </div>
-                            <div class="col-2 p-3">
-                                <img src="img/vodafone-cash.png" class="img-fluid" alt="...">
-                            </div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col-4">
-                                <p>
-                                <h3 class="text-center">SUPPORT <br> THE <br> KINGDOM</h3>
-                                <img src="img/Donate Page_GiveNow.png" class="img-fluid pt-4" alt="...">
-                                </p>
-                            </div>
-                            <div class="col-6">
-                                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="$_POST">
-                                    <?php
-                                    if (isset($_POST['submit'])) {
-                                        $phone = $_POST['phone'];
-                                        $amount = $_POST['amount'];
-                                    }
-                                    ?>
-                                    <div class=" p-5 mt-0">
-                                        <div class="form-floating ">
-                                            <div class=" mb-3 input-group">
-                                                <span class="input-group-text">Full Name</span>
-                                                <input type="text" aria-label="phone-number" name="fullname" class="form-control" placeholder="Jon Doe" required>
-                                            </div>
-                                            <div class=" mb-3 input-group">
-                                                <span class="input-group-text">Phone</span>
-                                                <input type="number" aria-label="phone-number" name="phone" class="form-control" placeholder="024XXXXXXX" required>
-                                            </div>
-                                            <div class=" mb-3 input-group">
-                                                <span class="input-group-text">Amount</span>
-                                                <input type="number" aria-label="amount" class="form-control" name="amount" id="amount" placeholder="0.00" required>
-                                            </div>
-                                        </div>
-
-                                        <div class="row justify-content-center">
-                                            <div class="col-sm-3 mb-3">
-                                                <button class="btn btn-md btn-outline-success" type="submit" name="submit">Give</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                                <div class="row justiy-content-center">
-                                    <div class="col-12">
-                                       
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div class="row justify-content-center">
+            <div class="col-sm-12 col-md-8 col-lg-6 m-auto">
+                <div class="p-3 hstack gap-3">
+                    <div class="col p-3">
+                        <img src="img/momo.jpg" class="img-fluid" alt="...">
+                    </div>
+                    <div class="col p-3">
+                        <img src="img/AirtelTigo-Money.png" class="img-fluid" alt="...">
+                    </div>
+                    <div class="col p-3">
+                        <img src="img/vodafone-cash.png" class="img-fluid" alt="...">
                     </div>
                 </div>
             </div>
         </div>
+        <div class="row justify-content-center">
+            <div class="col-6 p-5">
+                <img src="img/Donate Page_GiveNow.png" class="img-fluid w-100 h-auto" alt="...">
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="$_POST">
+                    <?php
+                    if (isset($_POST['submit'])) {
+                        $phone = $_POST['phone'];
+                        $amount = $_POST['amount'];
+                    }
+                    ?>
+                    <div class=" p-5 m-auto">
+                        <div class="form-floating ">
+                            <div class="col-4 mb-3 input-group">
+                                <span class="input-group-text">Full Name</span>
+                                <input type="text" aria-label="phone-number" name="fullname" class="form-control" placeholder="Jon Doe" required>
+                            </div>
+                            <div class="col-4 mb-3 input-group">
+                                <span class="input-group-text">Phone</span>
+                                <input type="number" aria-label="phone-number" name="phone" class="form-control" placeholder="024XXXXXXX" required>
+                            </div>
+                            <div class="col-4 mb-3 input-group">
+                                <span class="input-group-text">Amount</span>
+                                <input type="number" aria-label="amount" class="form-control" name="amount" id="amount" placeholder="0.00" required>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center m-auto">
+                            <div class="col-2 mb-3">
+                                <button class="btn btn-outline-success" type="submit" name="submit">Give</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </div>
-
 <?php
 include('components/footer.php');
 ?>
