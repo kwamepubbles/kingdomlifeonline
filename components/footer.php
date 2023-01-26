3c00dd7219ea3d48b0df1d74031401ad6f49e8c2
Elvis Taylor <taylorelvisedem@gmail.com>
2023-01-26 10:10:08 +0000

    background css update

--- components/footer.php
+++ components/footer.php
@@ -1,92 +1,90 @@
+<!-- Footer 
+<nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+  <ol class="breadcrumb justify-content-start mx-4">
+    <li class="breadcrumb-item"><a href="#"><i class="bi bi-house me-1"></i>Home</a></li>
+    <li class="breadcrumb-item"><a href="#"><i class="bi bi-headphones me-1"></i>Podcast</a></li>
+    <li class="breadcrumb-item"><a href="#"><i class="fas fa-camera me-1"></i>Gallery</a></li>
+    <li class="breadcrumb-item"><a href="#"><i class="bi bi-book me-1"></i>Library</a></li>
+    <li class="breadcrumb-item"><a href="#"><i class="fa-solid fa-blog me-1"></i></i>Blog</a></li>
+    <li class="breadcrumb-item"><a href="#"><i class="fa fa-praying-hands me-1"></i>Pray</a></li>
+    <li class="breadcrumb-item"><a href="#"><i class="fas fa-seedling me-1"></i>Seeds</a></li>
+    <li class="breadcrumb-item active" aria-current="page"><a href="#"><i class="fas fa-fax me-1"></i>Contact</a></li>
+  </ol>
+</nav>-->
+<footer>
+  <!--Grid container-->
+  <div class="container-fluid text-white pt-2" style="background-color: #8f6304;">
+    <!--Grid row-->
+    <div class="row justify-content-center m-auto">
+      <!--Grid column-->
+      <div class="col-sm-12 col-md-4 col-lg-4  text-center m-auto">
+        <div><i class="fas fa-church"></i></div>
+        <h6 class="text-uppercase">Programs</h6>
+        <ul class="list-unstyled">
+          <li><a href="index.php" class="text-white">School of the Spirit</a></li>
+          <li><a href="index.php" class="text-white">Solution Night</a></li>
+          <li><a href="index.php" class="text-white">Mountain Experience</a></li>
+        </ul>
+      </div>
+      <!--Grid column-->
+      <!--Grid column-->
+      <div class="col-sm-12 col-md-4 col-lg-4 text-center m-auto">
+        <div><i class="fas fa-music"></i></div>
+        <h6 class="text-uppercase">Media </h6>
+        <ul class="list-unstyled">
+          <li><a href="gallery.php" class="text-white">Gallery</a></li>
+          <li><a href="podcast.php" class="text-white">Podcasts</a></li>
+          <li><a href="library.php" class="text-white">Library</a></li>
+        </ul>
+      </div>
+      <!--Grid column-->
+      <!--Grid column-->
+      <div class="col-sm-12 col-md-4 col-lg-4 text-center m-auto">
+        <div><i class="fas fa-headset"></i></div>
+        <h6 class="text-uppercase">Contact </h6>
+        <ul class="list-unstyled">
+          <li class="text-break"><a><i class="fas fa-home"></i> University of Ghana, Legon. Accra-GH</a></li>
+          <li><a><i class="fas fa-envelope"></i> kingdomlife2233@gmail.com</a></li>
+          <li><a><i class="fas fa-phone"></i> + 233-234-567-88</a></li>
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+  <!-- Section: Social media -->
+  <!-- Right -->
+  <div class="container-fluid social">
+    <div class="row justify-content-evenly mx-auto">
+      <div class="col-sm-12 col-md-12 col-lg-6 ">
+        <h2 class="p-0 m-0  text-white"><i class="fas fa-link"></i> Connect With Us :</h2>
+      </div>
+      <div class="col-sm-12 col-md-12 col-lg-6 m-auto">
+        <nav class="navbar">
+          <ul class="navbar list-unstyled col-6 m-auto">
+            <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-whatsapp"></i></a></li>
+            <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-telegram"></i></a></li>
+            <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fa-solid fa-city"></i></a></li>
+            <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-youtube"></i></a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <!-- Right -->
+  <!-- Copyright -->
+  <div class="text-center text-white p-1 copyright ">
+    <?php echo "©";
+    echo date("Y");
+    echo " Copyright."; ?><br>
+    <a class="text-white" href="#">Data Protection and Privacy Policy</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 <!-- Footer -->
-<section>
-  <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
-    <ol class="breadcrumb justify-content-start ms-4 me-4">
-      <li class="breadcrumb-item"><a href="#"><i class="bi bi-house me-1"></i>Home</a></li>
-      <li class="breadcrumb-item"><a href="#"><i class="bi bi-headphones me-1"></i>Podcast</a></li>
-      <li class="breadcrumb-item"><a href="#"><i class="fas fa-camera me-1"></i>Gallery</a></li>
-      <li class="breadcrumb-item"><a href="#"><i class="bi bi-book me-1"></i>Library</a></li>
-      <li class="breadcrumb-item"><a href="#"><i class="fa-solid fa-blog me-1"></i></i>Blog</a></li>
-      <li class="breadcrumb-item"><a href="#"><i class="fa fa-praying-hands me-1"></i>Pray</a></li>
-      <li class="breadcrumb-item"><a href="#"><i class="fas fa-seedling me-1"></i>Seeds</a></li>
-      <li class="breadcrumb-item active" aria-current="page"><a href="#"><i class="fas fa-fax me-1"></i>Contact</a></li>
-    </ol>
-  </nav>
-  <footer>
-    <!--Grid container-->
-    <div class="container-fluid text-white" style="background-color: #8f6304;">
-      <!--Grid row-->
-      <div class="row justify-content-center pt-2 m-auto">
-        <!--Grid column-->
-        <div class="col-sm-12 col-md-4 col-lg-4  text-center m-auto">
-          <div><i class="fas fa-church"></i></div>
-          <h6 class="text-uppercase">Ministrations</h6>
-          <ul class="list-unstyled m-auto">
-            <li><a href="index.php" class="text-white">School of the Spirit</a></li>
-            <li><a href="index.php" class="text-white">Solution Night</a></li>
-            <li><a href="index.php" class="text-white">Mountain Experience</a></li>
-          </ul>
-        </div>
-        <!--Grid column-->
-        <!--Grid column-->
-        <div class="col-sm-12 col-md-4 col-lg-4 text-center m-auto">
-          <div><i class="fas fa-music"></i></div>
-          <h6 class="text-uppercase">Media </h6>
-          <ul class="list-unstyled">
-            <li><a href="gallery.php" class="text-white">Gallery</a></li>
-            <li><a href="podcast.php" class="text-white">Podcasts</a></li>
-            <li><a href="library.php" class="text-white">Library</a></li>
-          </ul>
-        </div>
-        <!--Grid column-->
-        <!--Grid column-->
-        <div class="col-sm-12 col-md-4 col-lg-4 text-center m-auto">
-          <div><i class="fas fa-headset"></i></div>
-          <h6 class="text-uppercase">Contact </h6>
-          <ul class="list-unstyled">
-            <li class="text-break"><a><i class="fas fa-home"></i> University of Ghana, Legon. Accra-GH</a></li>
-            <li><a><i class="fas fa-envelope"></i> kingdomlife2233@gmail.com</a></li>
-            <li><a><i class="fas fa-phone"></i> + 233-234-567-88</a></li>
-          </ul>
-        </div>
-        <!--Grid column-->
-      </div>
-      <!--Grid row-->
-    </div>
-    <!-- Grid container -->
-    <!-- Section: Social media -->
-    <!-- Right -->
-    <div class="container-fluid text-white" style="background-color: #380958;">
-      <div class="row justify-content-evenly m-auto">
-        <div class="col-sm-12 col-md-12 col-lg-6 ">
-          <h2 class="p-0 m-0  text-white"><i class="fas fa-link"></i> Connect With Us :</h2>
-        </div>
-        <div class="col-sm-12 col-md-12 col-lg-6 m-auto">
-          <nav class="navbar">
-            <ul class="navbar list-unstyled col-6 m-auto">
-              <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-facebook-f"></i></a></li>
-              <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-whatsapp"></i></a></li>
-              <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-telegram"></i></a></li>
-              <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fas fa-home"></i></a></li>
-              <li class="nav-item"><a href="#" role="button" class="nav-link"><i class="fab fa-youtube"></i></a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <!-- Right -->
-    <!-- Copyright -->
-    <div class="text-center text-white p-1" style="background-color: #37343b;">
-      <?php echo "©";
-      echo date("Y");
-      echo " Copyright."; ?><br>
-      <a class="text-white" href="#">Data Protection and Privacy Policy</a>
-    </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
-  <script src="assets/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  </body>
-
-  </html>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,15 +8,14 @@ require_once('scripts/db.config.php');
 include('components/carousel.php');
 ?>
 
-<section>
-    <div class="container pt-4">
-        <div class="row g-1">
-            <div class="col-md-6 mb-md-0 p-md-4">
-
-                <div class="card-body border rounded p-2 bg-light">
-                    <img class="img-fluid w-50" src="img/klm-HD.png" alt="...">
-                    <p class="text-break">
-                    <h6>Welcome to Kingdom Life!</h6> Our mission is to spread the word of God and provide resources and support for those seeking a deeper connection with their faith.
+<section class="welcome">
+    <div class="container-fluid pt-4">
+        <div class="row mb-0 p-4 m-auto">
+            <img class="d-block w-25" src="img/klm-HDlight.png" alt="...">
+            <div class="p-4 text-bg-dark">
+                <h4>Welcome to Kingdom Life!</h4>
+                <p class="text-break">
+                    Our mission is to spread the word of God and provide resources and support for those seeking a deeper connection with their faith.
 
                     On our website, you'll find a variety of resources including daily devotions and sermons. We also have a section dedicated to prayer and to help you connect with God.
 
@@ -30,15 +29,16 @@ include('components/carousel.php');
 
                     In Christ, <br>
                     [Kingdomlife Team]</p>
-
-                </div>
             </div>
-            <div class="col-md-6 mb-md-0 p-md-4">
-                <div class="p-2 bg-success rounded">
-                    <h2 class=" text-center"> Daily Nuggets</h2>
-                    <div class="card-body border">
-                        <h4 class="text-center text-bg-dark">Post title</h4>
-                        <p class="text-break">
+        </div>
+
+        <div class="row g-2 m-4">
+            <div class="col-md-6 mb-md-0 p-md-4 m-auto">
+                <div class="p-2" style="background-color: rgba(42, 2, 48, 0.747);">
+                    <h2 class=" text-center my-4 text-white"> Daily Nuggets</h2>
+                    <div class="card-body p-4">
+                        <h4 class="text-center text-bg-dark p-2">Post Title</h4>
+                        <p class="text-break text-white mb-4">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in semper tellus. Donec a elit vel massa elementum malesuada. Mauris lacus lectus, aliquam quis ultricies ac, pretium in leo. Donec dictum, massa id luctus facilisis, diam est malesuada massa, ac scelerisque arcu tellus at est.
 
                             Aliquam id neque sed justo faucibus ornare. Duis quis dictum dui. In scelerisque lacus in tortor pellentesque faucibus. Nulla rhoncus facilisis dui, in posuere diam. Nulla a tortor non metus dictum pharetra. Duis euismod, tortor ut rhoncus vehicula, lacus neque pulvinar lectus, et condimentum massa nulla id tortor.
@@ -46,42 +46,36 @@ include('components/carousel.php');
                             Curabitur a condimentum purus, eu viverra nisi. Morbi pellentesque scelerisque tellus, et laoreet nisl mollis in.
                         </p>
                     </div>
-                    <div class="row g-1 bg-light position-relative">
-                        <div class="col-md-6 mb-md-0 p-md-4">
-                            <img src="img/calm-spiritual.jpg" class="w-100" alt="...">
-                        </div>
-                        <div class="col-md-6 p-0 ps-md-0">
-                            <h5 class="mt-0">Columns with stretched link</h5>
-                            <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
-                            <a href="#" class="stretched-link">Go somewhere</a>
-                        </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mb-md-0 p-md-4 m-auto">
+                <div class="p-0" style="background-color: rgba(42, 2, 48, 0.747);">
+                        <img src="img/papa.png" class="d-block w-75 position-relative bottom-0 end-0 me-0" alt="">
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<section>
-    <div class="container mt-4 overflow-hidden text-center">
-        <h2 class="text-center my-4 ">Sessions & Events</h2>
+
+<section class="sessions">
+    <div class="container-fluid mt-4 overflow-hidden text-center">
+        <h2 class="text-center my-4 ">Our Programs</h2>
         <div class="row gx-5 pt-4">
             <div class="col-sm-12 col-md-12 col-lg-4 m-auto">
                 <img src="img/sots.jpg" class="d-block w-75 h-auto m-auto p-2" alt="...">
             </div>
             <div class="col-sm-12 col-md-12 col-lg-4 m-auto">
-                <img src="img/sn.jpg" class="img-fluid d-block w-75 h-auto m-auto p-2" alt="...">
+                <img src="img/sn2.jpg" class="img-fluid d-block w-75 h-auto m-auto p-2" alt="...">
             </div>
             <div class="col-sm-12 col-md-12 col-lg-4 m-auto">
-                <img src="img/sn.jpg" class="img-fluid d-block w-75 h-auto m-auto p-2" alt="...">
+                <img src="img/me23.jpg" class="img-fluid d-block w-75 h-auto m-auto p-2" alt="...">
             </div>
         </div>
     </div>
 </section>
-<section>
-    <div class="container mt-4 pb-4">
 
-    </div>
-</section>
 <?php
 include('events.php');
 ?>
