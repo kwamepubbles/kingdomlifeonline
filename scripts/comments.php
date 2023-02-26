3ddc8fa19900b12d5614fe163984d34d79d2e254
Elvis Taylor <taylorelvisedem@gmail.com>
2023-02-26 15:40:47 +0000

    blog comment system

--- scripts/comments.php
+++ scripts/comments.php
@@ -1,5 +1,5 @@
 <?php
-
+include('db.config.php');
 function time_elapsed_string($datetime, $full = false)
 {
 
@@ -9,7 +9,7 @@ function time_elapsed_string($datetime, $full = false)
     $diff->w = floor($diff->d / 7);
     $diff->d -= $diff->w * 7;
     $string = array(
-        'y' => 'year', 'm' => 'month', 'W' => ' week', 'd' => 'day',
+        'y' => 'year', 'm' => 'month', 'w' => ' week', 'd' => 'day',
         'h' => 'hour', 'i' => 'minute', 's' => 'second'
     );
 
@@ -91,15 +91,128 @@ function show_comment($comment, $comments = [], $filters = [])
 }
 
 //
-function show_comments($comments, $filters, $parent_id = -1){
-  $html = '';
-  if($parent_id != -1){
-    array_multisort(array_column($comments, 'submit_date'), SORT_ASC, $comments);
-   }//End of if
+function show_comments($comments, $filters, $parent_id = -1)
+{
+    $html = '';
+    if ($parent_id != -1) {
+        array_multisort(array_column($comments, 'submit_date'), SORT_ASC, $comments);
+    } //End of if
     foreach ($comments as $comment) {
-        if ($comment ['parent_id'] == $parent_id) {
+        if ($comment['parent_id'] == $parent_id) {
             $html .= show_comment($comment, $comments, $filters);
         }
-    }//End of foreach
+    } //End of foreach
     return $html;
-}//End of function
+} //End of function
+
+function show_write_comment_form($parent_id = -1)
+{
+    $html = '
+       <div class="write_comment" data-comment-id="' .  $parent_id . '"
+            <form>
+                <input name="parent_id" type= "hidden" value="' . $parent_id . '">
+                <input name="name" type="text" placeholder= "Your Name" required>
+                <textarea name="content" placeholder="Write your comment here..." required</textarea>
+                <input name="img_url" type="file" placeholder="(optional)">
+                <button type="submit">Submit</button>
+            </form>
+       </div>';
+    return $html;
+} //End of function 
+
+// Page ID
+//Retrieve the $filters
+$stmt = $plug->prepare('SELECT * FROM filters');
+$stmt->execute();
+$filters = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//
+if (isset($_POST['name'], $_POST['content'], $_POST['parent_id'], $_POST['img_url'])) {
+
+    //Insert a new comment
+    $stmt = $plug->prepare('INSERT INTO comments (page_id, parent_id, name, content,
+       submit_date, img, comments_approval_required) VALUES (?,?,?,?,NOW(),?,?)');
+    $approved = COMMENTS_APPROVAL_REQUIRED ? 0 : 1;
+    $stmt->execute([$_GET['page_id'], $_POST['parent_id'], $_POST['name'], $_POST['content'], $_POST['img_url'], $approved]);
+
+    //Retrieve the comment
+    $stmt = $plug->prepare('SELECT * FROM comments WHERE id = ?');
+    $stmt->execute([$plug->lastInsertId()]);
+    $comment = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //Output the comment
+    exit(show_comment($comment));
+}
+
+//Vote buttons
+if (isset($_GET['vote'], $_GET['comment_id'])) {
+    $stmt = $plug->prepare('UPDATE comments SET votes = votes + 1 WHERE id = ?');
+    //cookie exists for this comment
+    if (!isset($_COOKIE['vote_' . $_GET['comment_id']])) {
+        $stmt = $plug->prepare('UPDATE comments SET votes = votes' . ($_GET['vote'] == 'up' ? '+' : '-') . ' 1 WHERE id = ?');
+        $stmt->execute([$_GET['comment_id']]);
+        //Set cookie to prevent users from voting multiple times
+        setcookie('vote_' . $_GET['comments_id'], 'true', time() + (10 * 365 * 24 * 60 * 60), '/');
+    } //End of cookie
+    //Retrieve the number of vote
+    $stmt = $plug->prepare('SELECT votes FROM comments WHERE id ');
+    $stmt->execute([$_GET['comment_id']]);
+    $comment = $stmt->fetch(PDO::FETCH_ASSOC);
+    //Output the votes
+    exit($comment['votes']);
+}
+
+//
+$comments_per_pagination_page = isset($_GET['comments_to_show']) ? $_GET['comments_to_show'] : 30;
+$limit = isset($_GET['current_pagination_page']) ? 'LIMIT :current_pagination_page' : '';
+//
+$sort_by = 'ORDER BY votes DESC, submit_date DESC';
+if (isset($_GET['sort_by'])) {
+    //User has changed the sort by
+    $sort_by = $_GET['sort_by'] == 'newest' ? 'ORDER BY submit_date DESC' : $sort_by;
+    $sort_by = $_GET['sort_by'] == 'oldest' ? 'ORDER BY submit_date ASC' : $sort_by;
+    $sort_by = $_GET['sort_by'] == 'Votes' ? 'ORDER BY submit_date DESC, submit_date DESC' : $sort_by;
+}
+
+//Get all comments by the page Id
+$stmt->$plug->prepare('SELECT * FROM comments WHERE page_id = :page_id AND approved = 1' . $sort_by . ' ' . $limit);
+if ($limit) {
+    $stmt->bindValue(':current_pagination_page', (int)$_GET['current_pagination_page'] * (int)$comments_per_pagination_page, PDO::PARAM_INT);
+
+    //bIND THE PAGE ID TO OUR QUERY
+    $stmt->bindvalue('page_id', $_GET['page_id'], PDO::PARAM_INT);
+    $stmt->execute();
+    $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    //Get total number of comments
+    $stmt = $plug->prepare('SELECT COUNT(*) AS total_comments FROM comments WHERE page_id = ? AND approved = 1');
+    $stmt->execute([$_GET['page_id']]);
+    $comments_info = $stmt->fetch(PDO::FETCH_ASSOC);
+} else {
+    exit('No page ID specified!');
+}
+
+?>
+<!--End of php -->
+<div class="comment_header">
+    <span class="total"><?= $comments_info['total_comments'] ?> comments</span>
+    <form>
+        <label for="sort_by"></label>
+        <select name="sort_by" id="sort_by">
+            <option value="" disabled<?= !isset($_GET['sort_by']) ? 'selected' : '' ?>>Sort By</option>
+            <option value="votes" <?= isset($_GET['sort_by']) && $_GET['sort_by'] == 'votes' ? 'selected' : '' ?>>Votes</option>
+            <option value="newest" <?= isset($_GET['sort_by']) && $_GET['sort_by'] == 'newest' ? 'selected' : '' ?>>Newest</option>
+            <option value="oldest" <?= isset($_GET['sort_by']) && $_GET['sort_by'] == 'oldest' ? 'selected' : '' ?>>Oldest</option>
+        </select>
+    </form>
+    <a href="#" class="write_comment_btn" data-comment-id="-1">Write Comment</a>
+</div>
+
+<?= show_write_comment_form() ?>
+
+<div class="comments_wrapper">
+    <?= show_comments($comments, $filters) ?>
+</div>
+
+<?php if (count($comments) < $comments_info['total_comments']) : ?>
+    <a href="#" class="show_more_comments">Show More</a>
+<?php endif; ?>
